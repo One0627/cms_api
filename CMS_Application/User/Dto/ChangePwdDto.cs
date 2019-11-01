a47accd11cee7628cedcb7250f2e0f62c947d16d
songlun <79060087@qq.com>
2019-11-01 09:27:09 +0800

    add data validation

--- CMS_Application/User/Dto/ChangePwdDto.cs
+++ CMS_Application/User/Dto/ChangePwdDto.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CMS_Application.User.UserInfo.Dto
 {
     public class ChangePwdDto
     {
+        [Required(ErrorMessage ="账号不能为空")]
         public string userNo { get; set; }
+        [Required(ErrorMessage = "旧密码不能为空")]
         public string oldPwd { get; set; }
+        [Required(ErrorMessage = "新密码不能为空")]
         public string newPwd { get; set; }
         public DateTime dateTime { get; set; }
         public string updateBy { get; set; }
--- CMS_Application/User/Dto/UserLoginInputDto.cs
+++ CMS_Application/User/Dto/UserLoginInputDto.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CMS_Application.User.Dto
 {
     public class UserLoginInputDto
     {
+        [Required(ErrorMessage ="用户名不能为空")]
         public string account { get; set; }
+        [Required(ErrorMessage ="密码不能为空")]
         public string passWord { get; set; }
     }
 }
